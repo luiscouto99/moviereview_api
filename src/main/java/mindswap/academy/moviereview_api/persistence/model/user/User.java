b8d69c94d0c1c71d2dd8bd3bbd07042443ac51e2
Utilizador <johnny.61576.js@gmail.com>
2022-06-30 10:14:19 +0100

    User 0.0

--- src/main/java/mindswap/academy/moviereview_api/persistence/model/user/User.java
+++ src/main/java/mindswap/academy/moviereview_api/persistence/model/user/User.java
@@ -2,16 +2,23 @@ package mindswap.academy.moviereview_api.persistence.model.user;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
+@NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "roleId", referencedColumnName = "id")
+    private String role;
 }
--- src/main/java/mindswap/academy/moviereview_api/persistence/model/user/role/Role.java
+++ src/main/java/mindswap/academy/moviereview_api/persistence/model/user/role/Role.java
@@ -1,15 +1,29 @@
 package mindswap.academy.moviereview_api.persistence.model.user.role;
 
 import lombok.*;
+import mindswap.academy.moviereview_api.persistence.model.user.User;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
-//@NoArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
-//@Entity
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String role;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY,
+            cascade = CascadeType.PERSIST)
+    private List<User> users;
 }
