812e4a2f87435e8aa2370305a2b073321e1a4cec
Utilizador <johnny.61576.js@gmail.com>
2022-07-01 19:08:34 +0100

    User 0.0

--- src/main/java/mindswap/academy/moviereview_api/command/user/UserUpdateDto.java
+++ src/main/java/mindswap/academy/moviereview_api/command/user/UserUpdateDto.java
@@ -3,15 +3,26 @@ package mindswap.academy.moviereview_api.command.user;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.*;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateDto {
+
+    @Min(0)
     private Long roleId;
+
+    @Size(max = 20)
     private String firstName;
+
+    @Size(max = 20)
     private String lastName;
+
+    @Email
+    @Size(max = 40)
     private String email;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
